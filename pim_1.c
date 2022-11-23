8e81879a091e85944643f362d2200d76e9706663
Stephen Justin <stevejustin1963@gmail.com>
2022-11-23 14:24:16 +1100

    Create pim_1.c

---
+++ pim_1.c
@@ -0,0 +1,49 @@
+// PIM; Pulse Interleaving Method. ...
+// is ingenious ...the limit medium can be pushed...a special high frequency is used to between 
+// high and low states fast enough to not allow the speaker cone to 
+// fully extend or collapse and instead keep it in one or more in-between states. 
+// Given enough in-between states one can represent a complex sine wave 
+// summation as a "stepped" rectangular wave and effectively achieve polyphony.
+
+// pseudo code for the above
+
+PIM(Pulse Interleaving Method):
+
+1. Use a high frequency to switch between high and low states.
+2. Keep the speaker cone in one or more in-between states.
+3. Represent a complex sine wave summation as a "stepped" rectangular wave.
+4. Achieve polyphony.
+
+
+// expand the above into each algorithm
+
+1. Use a high frequency to switch between high and low states:
+
+-Set a high frequency to switch between high and low states.
+-Keep the speaker cone in one or more in-between states.
+-Represent a complex sine wave summation as a "stepped" rectangular wave.
+-Achieve polyphony.
+
+2. Keep the speaker cone in one or more in-between states:
+
+-Set a high frequency to switch between high and low states.
+-Keep the speaker cone in one or more in-between states.
+-Represent a complex sine wave summation as a "stepped" rectangular wave.
+-Achieve polyphony.
+
+3. Represent a complex sine wave summation as a "stepped" rectangular wave:
+
+-Set a high frequency to switch between high and low states.
+-Keep the speaker cone in one or more in-between states.
+-Represent a complex sine wave summation as a "stepped" rectangular wave.
+-Achieve polyphony.
+
+4. Achieve polyphony:
+
+-Set a high frequency to switch between high and low states.
+-Keep the speaker cone in one or more in-between states.
+-Represent a complex sine wave summation as a "stepped" rectangular wave.
+-Achieve polyphony.
+
+ 
+ 
