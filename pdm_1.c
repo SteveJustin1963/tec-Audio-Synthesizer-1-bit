a6902f24a9abe1866162395590e733a6ab91c4cc
Stephen Justin <stevejustin1963@gmail.com>
2023-09-14 15:22:18 +1000

    Update pdm_1.c

--- pdm_1.c
+++ pdm_1.c
@@ -1,73 +1,51 @@
-//Produce s samples of p periods of a sine wave
- function sample(int s, int p)
-   s := s - 1
-   var real ω := p × π × 2 ÷ s
-   var real[0..s] pcm
-   for i from 0 to s
-       pcm[i] := sin(ω × i)
-   return pcm
- 
- //Encode samples into pulse-density modulation
- function encode(int[0..s] pcm)
-   var real[0..s] pdm
-   var real r := 1
-   for i from 0 to s
-       r := pcm[i] - r
-       if r > 0
-           pdm[i] := 1
-           r := 1 - r
-       else
-           pdm[i] := 0
-           r := -1 - r
-   return pdm
+#include <math.h>
+#include <stdlib.h>
 
-convert the above to c code
+#define PI 3.14159265358979323846
 
+// Produce s samples of p periods of a sine wave
+float* sample(int s, int p) {
+    s = s - 1;
+    float omega = (float) p * PI * 2 / s;
+    float* pcm = (float*) malloc((s + 1) * sizeof(float));
 
-int sample(int s, int p)
-{
-   s = s - 1;
-   float ω = p * π * 2 / s;
-   float[] pcm;
-   for (int i = 0; i <= s; i++)
-       pcm[i] = sin(ω * i);
+    for (int i = 0; i <= s; i++) {
+        pcm[i] = sin(omega * i);
+    }
 
-
-   return pcm;
-}
- 
-int encode(float[] pcm)
-{
-   float[] pdm;
-   float r = 1;
-   for (int i = 0; i <= s; i++)
-   {
-       r = pcm[i] - r;
-       if (r > 0)
-       {
-           pdm[i] = 1;
-           r = 1 - r;
-       }
-       else
-       {
-           pdm[i] = 0;
-           r = -1 - r;
-       }
-   }
-   return pdm;
+    return pcm;
 }
 
+// Encode samples into pulse-density modulation
+float* encode(float* pcm, int s) {
+    float* pdm = (float*) malloc((s + 1) * sizeof(float));
+    float r = 1.0;
 
-convert to forth 83 code
+    for (int i = 0; i <= s; i++) {
+        r = pcm[i] - r;
+        if (r > 0) {
+            pdm[i] = 1;
+            r = 1 - r;
+        } else {
+            pdm[i] = 0;
+            r = -1 - r;
+        }
+    }
 
+    return pdm;
+}
 
-: sample ( s p -- pcm )
-   1- s p π 2 f* p f/ s f/range sin
-;
-: encode ( pcm -- pdm )
-   1.0 r>
-   pdm 1
-   r>
-   pdm 0
-   r>
-;
+int main() {
+    // Test the functions
+    int s = 100, p = 2;
+    float* pcm = sample(s, p);
+    float* pdm = encode(pcm, s);
+
+    // Do something with pcm and pdm arrays
+
+    // Free the allocated memory
+    free(pcm);
+    free(pdm);
+
+    return 0;
+}
