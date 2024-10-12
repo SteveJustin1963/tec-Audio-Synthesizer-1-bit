898f61e1f1e178e22494bbb8234a43521ad6c31a
Stephen Justin <stevejustin1963@gmail.com>
2024-10-12 23:28:00 +1100

    Update pdm_1.c

--- pdm_1.c
+++ pdm_1.c
@@ -49,3 +49,37 @@ int main() {
 
     return 0;
 }
+
+
+;;;;;;;;;;;;;;;;;;;;;;
+ ; Z80 Assembly code to output 8-bit sine wave values to port 0x12
+
+    ORG 0x0000           ; Start of the program
+
+    LD HL, SINE_TABLE    ; HL points to the sine wave table
+    LD C, 0x12           ; C holds the DAC port address (0x12)
+    LD B, 100            ; B holds the number of samples (100)
+
+OUTPUT_LOOP:
+    LD A, (HL)           ; Load the next 8-bit sine value into A
+    OUT (C), A           ; Send the value to the DAC at port 0x12
+    INC HL               ; Move to the next value in the table
+    DJNZ OUTPUT_LOOP     ; Decrement B and loop if B != 0
+
+    HALT                 ; Stop the program
+
+; Pre-calculated 8-bit sine wave table, scaled from [-1, 1] to [0, 255]
+SINE_TABLE:
+    DB 128, 136, 144, 152, 159, 167, 174, 181, 188, 194
+    DB 200, 206, 211, 216, 221, 225, 229, 233, 236, 239
+    DB 241, 243, 244, 245, 246, 246, 246, 245, 244, 243
+    DB 241, 239, 236, 233, 229, 225, 221, 216, 211, 206
+    DB 200, 194, 188, 181, 174, 167, 159, 152, 144, 136
+    DB 128, 119, 111, 103,  96,  88,  81,  74,  67,  61
+    DB  55,  49,  44,  39,  34,  30,  26,  22,  19,  16
+    DB  14,  12,  11,  10,  10,  10,  10,  11,  12,  14
+    DB  16,  19,  22,  26,  30,  34,  39,  44,  49,  55
+    DB  61,  67,  74,  81,  88,  96, 103, 111, 119, 128
+
+    END                  ; End of program
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
